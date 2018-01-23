1f8d2126c3d03f33ae2a204890e2bfd7de8b3a63
norablakaj <nora.blakaj@leg-uelzen.de>
2018-01-23 09:14:55 +0100

    Vokabeltrainer

---
+++ LateinApp/app/src/main/java/com/example/norablakaj/lateinapp/Vokabeltrainer_Lektion_1.java
@@ -0,0 +1,13 @@
+package com.example.norablakaj.lateinapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Vokabeltrainer_Lektion_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vokabeltrainer__lektion_1);
+    }
+}
