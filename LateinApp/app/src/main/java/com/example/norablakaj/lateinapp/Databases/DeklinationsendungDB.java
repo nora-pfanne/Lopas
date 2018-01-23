c0f68c4bf5ddcef3db100f41af22c3d327df0bcd
norablakaj <nora.blakaj@leg-uelzen.de>
2018-01-23 08:07:42 +0100

    Databases

--- LateinApp/app/src/main/java/com/example/norablakaj/lateinapp/Databases/DeklinationsendungDB.java
+++ LateinApp/app/src/main/java/com/example/norablakaj/lateinapp/Databases/DeklinationsendungDB.java
@@ -22,10 +22,10 @@ public class DeklinationsendungDB {
                                     COLUMN_GEN_SG = "Gen_Sg",
                                     COLUMN_GEN_PL = "Gen_Pl",
                                     COLUMN_DAT_SG = "Dat_Sg",
-                                    COLUMN_DAT = "Nom_Sg",
-                                    COLUMN_NOM_SG = "Nom_Sg",
-                                    COLUMN_NOM_SG = "Nom_Sg",
-                                    COLUMN_NOM_SG = "Nom_Sg",
-                                    COLUMN_NOM_SG = "Nom_Sg",
+                                    COLUMN_DAT_PL = "Dat_Pl",
+                                    COLUMN_AKK_SG = "Akk_Sg",
+                                    COLUMN_AKK_PL = "Akk_PL",
+                                    COLUMN_ABL_SG = "Abl_Sg",
+                                    COLUMN_ABL_PL = "Abl_Pl";
     }
 }
