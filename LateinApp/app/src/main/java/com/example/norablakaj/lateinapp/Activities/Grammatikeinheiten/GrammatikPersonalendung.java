4fa77435fcd658e21a22808581f072127a0144b0
norablakaj <nora.blakaj@leg-uelzen.de>
2018-02-19 21:52:02 +0100

    Starting new Activity

--- LateinApp/app/src/main/java/com/example/norablakaj/lateinapp/Activities/Grammatikeinheiten/GrammatikPersonalendung.java
+++ LateinApp/app/src/main/java/com/example/norablakaj/lateinapp/Activities/Grammatikeinheiten/GrammatikPersonalendung.java
@@ -1,8 +1,15 @@
 package com.example.norablakaj.lateinapp.Activities.Grammatikeinheiten;
 
-/**
- * Created by Nora Blakaj on 15.02.2018.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class GrammatikPersonalendung {
+import com.example.norablakaj.lateinapp.R;
+
+public class GrammatikPersonalendung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grammatik_personalendung);
+    }
 }
