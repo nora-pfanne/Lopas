88328b51452943ffdd755894241f0865c7cc8ca0
Zeldacrafter <ayko12345@gmail.com>
2018-02-19 11:21:15 +0000

    Making the developer-switch appear with a num-code 2-2-1-3-4

--- LateinApp/app/src/main/java/com/example/norablakaj/lateinapp/Activities/SettingsActivity.java
+++ LateinApp/app/src/main/java/com/example/norablakaj/lateinapp/Activities/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Switch;
 
 import com.example.norablakaj.lateinapp.Activities.Home;
@@ -13,6 +14,9 @@ public class SettingsActivity extends AppCompatActivity {
 
     Switch toggleDevModeSwitch;
 
+    //TODO: Make this into a enum
+    private int devSwitchState = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,24 @@ public class SettingsActivity extends AppCompatActivity {
         toggleDevModeSwitch = findViewById(R.id.toggleDevModeSwitch);
 
         toggleDevModeSwitch.setChecked(Home.getDeveloper());
+
+        toggleDevModeSwitch.setVisibility(View.GONE);
+
+        Button buttonTopLeft = findViewById(R.id.settingsInvisTopLeft);
+        Button buttonTopRight = findViewById(R.id.settingsInvisTopRight);
+        Button buttonBottomLeft = findViewById(R.id.settingsInvisBottomLeft);
+        Button buttonBottomRight = findViewById(R.id.settingsInvisBottomRight);
+
+        buttonTopLeft.setVisibility(View.INVISIBLE);
+        buttonTopRight.setVisibility(View.INVISIBLE);
+        buttonBottomLeft.setVisibility(View.INVISIBLE);
+        buttonBottomRight.setVisibility(View.INVISIBLE);
+        //TODO: Make this work for invisible buttons
+        buttonTopLeft.setVisibility(View.VISIBLE);
+        buttonTopRight.setVisibility(View.VISIBLE);
+        buttonBottomLeft.setVisibility(View.VISIBLE);
+        buttonBottomRight.setVisibility(View.VISIBLE);
+
     }
 
     public void switchPressed(View v){
@@ -29,4 +51,39 @@ public class SettingsActivity extends AppCompatActivity {
             Home.setDeveloper(toggleDevModeSwitch.isChecked());
         }
     }
+
+    public void settingsButtonClicked(View v){
+        int id = v.getId();
+        //Code: 2-2-3-1-4
+        if (id == R.id.settingsInvisTopLeft){
+            Log.d("Button1", "Button 1 was pressed");
+            if (devSwitchState == 3){
+                devSwitchState++;
+            }else{
+                devSwitchState = 0;
+            }
+        }else if (id == R.id.settingsInvisTopRight){
+            Log.d("Button2", "Button 2 was pressed");
+            if (devSwitchState == 0 || devSwitchState == 1){
+                devSwitchState++;
+            }else{
+                devSwitchState = 0;
+            }
+        }else if (id == R.id.settingsInvisBottomLeft){
+            Log.d("Button3", "Button 3 was pressed");
+            if (devSwitchState == 2){
+                devSwitchState++;
+            }else{
+                devSwitchState = 0;
+            }
+        }else if (id == R.id.settingsInvisBottomRight){
+            Log.d("Button4", "Button 4 was pressed");
+            if (devSwitchState == 4){
+                toggleDevModeSwitch.setVisibility(View.VISIBLE);
+            }else{
+                devSwitchState = 0;
+            }
+        }
+    }
+
 }
