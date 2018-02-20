cd4868aed82dab053f60169566c22e8c1415ee4d
norablakaj <nora.blakaj@leg-uelzen.de>
2018-02-20 07:40:33 +0100

    minor changes

--- LateinApp/app/src/main/java/com/example/norablakaj/lateinapp/Activities/Grammatikeinheiten/GrammatikPersonalendung.java
+++ LateinApp/app/src/main/java/com/example/norablakaj/lateinapp/Activities/Grammatikeinheiten/GrammatikPersonalendung.java
@@ -2,10 +2,28 @@ package com.example.norablakaj.lateinapp.Activities.Grammatikeinheiten;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.example.norablakaj.lateinapp.Databases.DBHelper;
 import com.example.norablakaj.lateinapp.R;
 
 public class GrammatikPersonalendung extends AppCompatActivity {
+
+    TextView grammatikUeberschrift;
+    TextView grammatikAufgabenstellung;
+    TextView latein;
+
+    Button erstePersSg, erstePersPl,
+            zweitwPersSg, zweitePersPl,
+            drittePersSg, drittePersPl;
+
+    ProgressBar progressBar;
+
+    DBHelper dbHelper;
+
+    int lektion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
