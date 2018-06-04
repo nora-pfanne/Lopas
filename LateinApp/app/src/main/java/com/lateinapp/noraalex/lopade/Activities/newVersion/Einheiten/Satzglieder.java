6e489fc188f1b50f217e1010ed340ca627aa3c47
norablakaj <nora.blakaj@leg-uelzen.de>
2018-06-04 08:14:19 +0200

    Transparent Button

--- LateinApp/app/src/main/java/com/lateinapp/noraalex/lopade/Activities/newVersion/Einheiten/Satzglieder.java
+++ LateinApp/app/src/main/java/com/lateinapp/noraalex/lopade/Activities/newVersion/Einheiten/Satzglieder.java
@@ -249,7 +249,7 @@ public class Satzglieder extends LateinAppActivity {
         int vocID;
 
         for (String s : currentSentence) {
-            Button button = new Button(this);
+            Button button = new Button(this, null, R.style.TransparentButton1);
 
             //FIXME: No try&catch with "NumberFormatException" if parseInt(String) fails
             // because it shouldnt possibly be able to fail
@@ -470,6 +470,8 @@ public class Satzglieder extends LateinAppActivity {
             //Adding spaces between the words so that they aren't directly next to eachother
             text += " ";
 
+
+            text = text.toLowerCase();
             button.setText(text);
 
             if (s.equals(elementToSelect)) {
