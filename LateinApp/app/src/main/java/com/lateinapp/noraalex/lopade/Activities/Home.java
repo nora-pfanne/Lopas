cb528511343186b349739a9529739fed0ac51d2c
Alexander Pfanne <ayko12345@gmail.com>
2018-10-12 18:48:00 +0200

    Creating BottomNavigationView for home screen

---
+++ LateinApp/app/src/main/java/com/lateinapp/noraalex/lopade/Activities/Home.java
@@ -0,0 +1,47 @@
+package com.lateinapp.noraalex.lopade.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.lateinapp.noraalex.lopade.R;
+
+public class Home extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.nav_vokabeltrainer:
+                    mTextMessage.setText("Vokabeltrainer");
+                    return true;
+                case R.id.nav_grammatik:
+                    mTextMessage.setText("Grammatik");
+                    return true;
+                case R.id.nav_wörterbuch:
+                    mTextMessage.setText("Wörterbuch");
+                    return true;
+
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
