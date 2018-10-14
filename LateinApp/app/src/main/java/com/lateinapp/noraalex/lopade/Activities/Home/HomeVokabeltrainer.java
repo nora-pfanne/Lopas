37032c8dd149958097c8c06cf5dadc283c32ac86
Alexander Pfanne <ayko12345@gmail.com>
2018-10-14 18:46:16 +0200

    Adding Homescreen info about progress / score

--- LateinApp/app/src/main/java/com/lateinapp/noraalex/lopade/Activities/Home/HomeVokabeltrainer.java
+++ LateinApp/app/src/main/java/com/lateinapp/noraalex/lopade/Activities/Home/HomeVokabeltrainer.java
@@ -1,19 +1,31 @@
 package com.lateinapp.noraalex.lopade.Activities.Home;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.lateinapp.noraalex.lopade.Activities.Einheiten.UserInputVokabeltrainer;
+import com.lateinapp.noraalex.lopade.General;
 import com.lateinapp.noraalex.lopade.R;
+import com.lateinapp.noraalex.lopade.Score;
 
 public class HomeVokabeltrainer extends Fragment {
 
     private static final String TAG = "HomeVokabeltrainer";
+
+    TextView tvScore1,
+            tvScore2,
+            tvScore3,
+            tvScore4,
+            tvScore5,
+            tvScore6;
+    SharedPreferences sharedPrefrences;
 
     public static HomeVokabeltrainer newInstance() {
         HomeVokabeltrainer fragment = new HomeVokabeltrainer();
@@ -30,8 +42,30 @@ public class HomeVokabeltrainer extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home_vokabeltrainer, container, false);
-        view.findViewById(R.id.home_voc_3).setEnabled(false);
+
+        tvScore1 = view.findViewById(R.id.home_voc_text_1);
+        tvScore2 = view.findViewById(R.id.home_voc_text_2);
+        tvScore3 = view.findViewById(R.id.home_voc_text_3);
+        tvScore4 = view.findViewById(R.id.home_voc_text_4);
+        tvScore5 = view.findViewById(R.id.home_voc_text_5);
+        tvScore6 = view.findViewById(R.id.home_voc_text_6);
+
+        sharedPrefrences = General.getSharedPrefrences(getActivity());
+
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        tvScore1.setText("HS: " + Score.getHighScoreVocabularyTrainer(sharedPrefrences,1) + "\nS: " + Score.getScoreVocabularyTrainer(1, sharedPrefrences));
+        tvScore2.setText("HS: " + Score.getHighScoreVocabularyTrainer(sharedPrefrences,2) + "\nS: " + Score.getScoreVocabularyTrainer(2, sharedPrefrences));
+        tvScore3.setText("HS: " + Score.getHighScoreVocabularyTrainer(sharedPrefrences,3) + "\nS: " + Score.getScoreVocabularyTrainer(3, sharedPrefrences));
+        tvScore4.setText("HS: " + Score.getHighScoreVocabularyTrainer(sharedPrefrences,4) + "\nS: " + Score.getScoreVocabularyTrainer(4, sharedPrefrences));
+        tvScore5.setText("HS: " + Score.getHighScoreVocabularyTrainer(sharedPrefrences,5) + "\nS: " + Score.getScoreVocabularyTrainer(5, sharedPrefrences));
+        tvScore6.setText("HS: " + Score.getHighScoreVocabularyTrainer(sharedPrefrences,6) + "\nS: " + Score.getScoreVocabularyTrainer(6, sharedPrefrences));
+
     }
 
     public void navButtonClicked(View v) {
@@ -39,37 +73,37 @@ public class HomeVokabeltrainer extends Fragment {
 
         switch (v.getId()) {
 
-            case R.id.home_voc_1:
+            case R.id.home_voc_button_1:
                 intent = new Intent(getActivity(), UserInputVokabeltrainer.class);
                 intent.putExtra("lektion",1);
                 startActivity(intent);
                 break;
 
-            case R.id.home_voc_2:
+            case R.id.home_voc_button_2:
                 intent = new Intent(getActivity(), UserInputVokabeltrainer.class);
                 intent.putExtra("lektion", 2);
                 startActivity(intent);
                 break;
 
-            case R.id.home_voc_3:
+            case R.id.home_voc_button_3:
                 intent = new Intent(getActivity(), UserInputVokabeltrainer.class);
                 intent.putExtra("lektion", 3);
                 startActivity(intent);
                 break;
 
-            case R.id.home_voc_4:
+            case R.id.home_voc_button_4:
                 intent = new Intent(getActivity(), UserInputVokabeltrainer.class);
                 intent.putExtra("lektion", 4);
                 startActivity(intent);
                 break;
 
-            case R.id.home_voc_5:
+            case R.id.home_voc_button_5:
                 intent = new Intent(getActivity(), UserInputVokabeltrainer.class);
                 intent.putExtra("lektion", 5);
                 startActivity(intent);
                 break;
 
-            case R.id.home_voc_6:
+            case R.id.home_voc_button_6:
                 intent = new Intent(getActivity(), UserInputVokabeltrainer.class);
                 intent.putExtra("lektion", 6);
                 startActivity(intent);
